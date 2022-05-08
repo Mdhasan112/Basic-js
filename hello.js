113f35c37ccf0efdfc5220d9c306046d01beb757
hasan <mdhasanhk.98@gmail.com>
2022-05-08 11:36:06 -0400

    first commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+var bananaPrice = 12;
+console.log(typeof bananaPrice)
+
+var apple = 10;
+console.log(typeof apple);
+
+var Name = "hasan khan";
+console.log(typeof Name);
+
+var thief = true;
+var honest = false;
+console.log(typeof thief);
