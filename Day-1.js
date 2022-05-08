15602999268bc6ea1ff4d6590156dc7b145bb2c1
hasan <mdhasanhk.98@gmail.com>
2022-05-08 11:41:27 -0400

    day-1

--- Day-1.js
+++ Day-1.js
@@ -1,12 +1,15 @@
+// number type variable
 var bananaPrice = 12;
 console.log(typeof bananaPrice)
 
 var apple = 10;
 console.log(typeof apple);
 
+// string type variable
 var Name = "hasan khan";
 console.log(typeof Name);
 
+// boolean type variable
 var thief = true;
 var honest = false;
 console.log(typeof thief);
