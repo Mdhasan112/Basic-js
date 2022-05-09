68c8e9681d97c08b4d300b6a24a00df416cad97a
hasan <mdhasanhk.98@gmail.com>
2022-05-09 15:46:01 -0400

    condition

---
+++ Day-2-conditionals.js
@@ -0,0 +1,21 @@
+var buiscuits = 17
+// <,>,==,!=
+if( buiscuits <15) {
+    console.log("I will eat the  buiscuits");
+}
+else{
+    console.log("I will not eat buiscuits");
+}
+
+var jobPaiso = true;
+var savings = 500000;
+// &&,||
+if(jobPaiso == true && savings > 20000) {
+    console.log("I will marry you");
+}
+else if(jobPaiso == true) {
+    console.log("I will try next tym")
+}
+else{
+    console.log("I will not marry you");
+}
