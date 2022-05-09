724cc82535af8b47a7c9ae3818ef17ac50171f66
hasan <mdhasanhk.98@gmail.com>
2022-05-09 04:57:31 -0400

    number

--- Day-1.js
+++ Day-1.js
@@ -13,3 +13,25 @@ console.log(typeof Name);
 var thief = true;
 var honest = false;
 console.log(typeof thief);
+
+var promise = "this is hasan. I am from BD";
+console.log(promise.toLowerCase());
+console.log(promise.toUpperCase());
+console.log(promise.indexOf("BD"));
+
+// integer variable
+var number1 = "20";
+// float variable
+var number2 = 10.5;
+
+// string variable theke number 
+var number1 = parseFloat(number1);
+// dosomik baad dite chaile parseint
+// var number1 = parseint(number1);
+console.log(number1+number2);
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total = number1+number2;
+total= total.toFixed(3);
+console.log(total);
