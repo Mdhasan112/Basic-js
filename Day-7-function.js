8c0cd90b3d3cca9e51066300268146b3dcb26016
hasan <mdhasanhk.98@gmail.com>
2022-05-12 15:29:25 -0400

    object

---
+++ Day-7-function.js
@@ -0,0 +1,16 @@
+function sayLoveYou () {
+    console.log("what is your name");
+}
+sayLoveYou();
+var age = 20;
+var place = "dhaka";
+sayLoveYou();
+
+function doubleIt (num) {
+    var result = num * 2;
+    return result;
+}
+var first = doubleIt(10);
+ var second = doubleIt(20);
+ var total = first + second
+ console.log(total);
---
+++ Day-8-object.js
@@ -0,0 +1,11 @@
+var student = {id: 121, phone: 1868, name: "mahim"};
+var student2 = {id: 131, phone: 1234, name: "rohim"};
+
+var phoneNo = student.phone;
+// or var phoneNo = student["phone"];
+
+// update phone
+student2.phone = 5555;
+
+console.log(phoneNo);
+console.log(student2);
