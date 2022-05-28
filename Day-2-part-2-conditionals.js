ebfb1c9fe922d9562566887e43a9161f3b0952c5
hasan <mdhasanhk.98@gmail.com>
2022-05-28 15:19:43 -0400

    conditionals

---
+++ Day-2-part-2-conditionals.js
@@ -0,0 +1,23 @@
+var a = 20;
+var b = 10;
+
+if(a > b) {
+    console.log("I love you")
+}
+else{
+    console.log("I hate you");
+}
+
+
+var x = 10;
+var y = 30;
+
+if( x < y ) {
+    console.log("I will be rich")
+}
+else if( x > y ) {
+    console.log("I will lost my asset")
+}
+else{
+    console.log("I have no change")
+}
