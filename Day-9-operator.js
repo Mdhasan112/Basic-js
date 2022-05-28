ab172bdd512cb62e0be1919499bad624914ed57e
hasan <mdhasanhk.98@gmail.com>
2022-05-28 13:57:03 -0400

    predefine-function

---
+++ Day-9-operator.js
@@ -0,0 +1,6 @@
+var num1 = 20;
+var num2 = 15;
+
+var total = num1 * num2; //(+, -, *, /, %)
+
+console.log(total);
---
+++ Day-9-part-2-predefine-function.js
@@ -0,0 +1,18 @@
+var num1 = -10;
+var sum = math.abs(num1);
+console.log(sum);
+
+var num1 = 0.4999;
+var sum = math.round(num1);
+console.log(sum);
+
+var num1 = 0.999;
+var sum = math.ceil(num1);
+console.log(sum);
+
+var num1 = 0.999;
+var sum = math.Floor(num1);
+console.log(sum);
+
+var lotary = math.random();
+console.log(lotary);
