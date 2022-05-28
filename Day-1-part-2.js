69fe0f4469557decdfa69091bc9040ba4fac8525
hasan <mdhasanhk.98@gmail.com>
2022-05-28 13:01:30 -0400

    day-1

---
+++ Day-1-part-2.js
@@ -0,0 +1,19 @@
+// string type theke number type convert
+
+var num1 = "20";
+var num2 = 15;
+
+num1 = parseFloat(num1); //parseInt use kora jay dosomik bad dewar jonno
+
+var total = num1 + num2;
+console.log(total);
+
+// number type theke string type convert
+
+var num3 = 20;
+var num4 = 10;
+
+num4 = "" + (num4);
+
+var total = num3 + num4;
+console.log(total);
