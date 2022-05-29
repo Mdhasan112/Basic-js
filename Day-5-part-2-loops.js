816bd08c81ddcbee0bca8dbc75a9c1e2120e5c19
hasan <mdhasanhk.98@gmail.com>
2022-05-29 06:01:55 -0400

    loops

---
+++ Day-5-part-2-loops.js
@@ -0,0 +1,15 @@
+for(var i = 1; i <= 5; i++ ) {
+    console.log("what is your name?")
+}
+
+var num = [1,2,3,4,5,6, "hasan", "rofiq"];
+
+for(var i = 0; i<num.length; i++) {
+    console.log(num[i]);
+}
+
+var i = 0;
+while( i < 10 ) {
+    console.log("hello");
+    i++;
+}
