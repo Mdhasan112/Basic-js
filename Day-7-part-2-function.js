c15bcb6c41984a1a329d66b676bd71e431db89b6
hasan <mdhasanhk.98@gmail.com>
2022-05-30 03:37:49 -0400

    function

---
+++ Day-7-part-2-function.js
@@ -0,0 +1,30 @@
+
+// function fun() {
+//     var num = 20;
+// var num2 = 10;
+
+// console.log(num * num2);
+// }
+
+// fun();
+// fun();
+// fun();
+
+function fun (num, num2){
+    var total = num + num2;
+    console.log(total);
+}
+
+fun(10, 20);
+fun(50, 100);
+
+function moja(num){
+    var total = num * 10;
+    return total;
+}
+
+var x = moja(10);
+var y = moja(20);
+
+console.log(x);
+console.log(y);
