b7775fcca01db4b47b07055b96a371954962e933
hasan <mdhasanhk.98@gmail.com>
2022-06-07 13:33:28 -0400

    apply.js

---
+++ apply-js/apply.js
@@ -0,0 +1,18 @@
+// var inch = 156;
+// var feet = inch/12;
+
+// console.log(feet);
+
+function inchToFeet(inch) {
+    var feet = inch/12;
+    return feet;
+}
+
+var dadaFeet = inchToFeet(156);
+console.log(dadaFeet);
+
+var dadiFeet = inchToFeet(300);
+console.log(dadiFeet);
+
+var mamaFeet = inchToFeet(250);
+ console.log(mamaFeet);
